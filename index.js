4e227e8410aecc666727c43d49a55caf215e3d6a
Ameesha Jain <ameesha.jain@nutrabay.com>
2024-01-18 13:17:22 +0530

    added mongodb and mongoose

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 import express from "express"
+import dotenv from 'dotenv'
+import mongoose from "mongoose"
 
+dotenv.config()
 const app = express()
 
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO)
+        console.log("connected to db");
+    } catch (e) {
+        throw e
+    }
+};
+
+mongoose.connection.on("disconnected", () => {
+    console.log("db disconnected");
+})
+
+mongoose.connection.on("connected", () => {
+    console.log("db connected");
+})
+
 app.listen(8000, () => {
+    connect()
     console.log("connected to backend :) !!");
 })
