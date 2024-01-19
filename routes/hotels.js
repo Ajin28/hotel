5997443040473cb85a9ce66663d2279686a4435a
Ameesha Jain <ameesha.jain@nutrabay.com>
2024-01-19 23:16:12 +0530

    added update api for hotel

--- routes/hotels.js
+++ routes/hotels.js
@@ -14,7 +14,17 @@ router.post("/", async (req, res) => {
         res.status(500).json(e)
     }
 })
+
 // update
+router.put("/:id", async (req, res) => {
+    try {
+        console.log(req.params.id);
+        const updateHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true })
+        res.status(200).json(updateHotel)
+    } catch (e) {
+        res.status(500).json(e)
+    }
+})
 // delete
 // get
 // get all
