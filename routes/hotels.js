43872c308189c206c0b4e111d229dd59d3ab16a6
Ameesha Jain <ameesha.jain@nutrabay.com>
2024-01-19 23:22:20 +0530

    added delete and get api for hotel

--- routes/hotels.js
+++ routes/hotels.js
@@ -18,15 +18,42 @@ router.post("/", async (req, res) => {
 // update
 router.put("/:id", async (req, res) => {
     try {
-        console.log(req.params.id);
         const updateHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true })
         res.status(200).json(updateHotel)
     } catch (e) {
         res.status(500).json(e)
     }
 })
+
 // delete
+router.delete("/:id", async (req, res) => {
+    try {
+        const deleteHotel = await Hotel.findByIdAndDelete(req.params.id)
+        res.status(200).json(deleteHotel)
+    } catch (e) {
+        res.status(500).json(e)
+    }
+})
+
 // get
+router.get("/:id", async (req, res) => {
+    try {
+        const getHotel = await Hotel.findById(req.params.id)
+        res.status(200).json(get)
+    } catch (e) {
+        res.status(500).json(e)
+    }
+})
+
 // get all
+router.get("/", async (req, res) => {
+    try {
+        const allHotel = await Hotel.find()
+        res.status(200).json(allHotel)
+    } catch (e) {
+        res.status(500).json(e)
+    }
+})
+
 
 export default router
