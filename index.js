f1fb1e31ce632d2a4d1603e6c779d569dcbd3a5e
Ameesha Jain <ameesha.jain@nutrabay.com>
2024-01-19 22:51:38 +0530

    added create api for hotel

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import express from "express"
 import dotenv from 'dotenv'
+import hotel_router from "./routes/hotels.js"
+import room_router from "./routes/rooms.js"
+import user_router from "./routes/users.js"
+import auth_router from "./routes/auth.js"
 import mongoose from "mongoose"
 
 dotenv.config()
@@ -21,6 +25,12 @@ mongoose.connection.on("disconnected", () => {
 mongoose.connection.on("connected", () => {
     console.log("db connected");
 })
+
+
+app.use(express.json())
+app.use("/hotel/v1", hotel_router)
+
+
 
 app.listen(8000, () => {
     connect()
