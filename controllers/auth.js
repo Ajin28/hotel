cb25b168a245b793a9c47acc9e4926cadffb5fb6
Ameesha Jain <ameesha.jain@nutrabay.com>
2024-01-20 12:50:31 +0530

    added bycrpt for hashing and salting password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,14 @@
 import User from '../models/User.js'
+import bcrypt from 'bcryptjs'
 
 export const register = async (req, res, next) => {
     try {
+
+        var salt = bcrypt.genSaltSync(10);
+        var hash = bcrypt.hashSync(req.body.password, salt);
         const newUser = new User({
             username: req.body.username,
-            password: req.body.password,
+            password: hash,
             email: req.body.email,
             phone: req.body.phone,
             city: req.body.city,
