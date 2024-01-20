8a6b13d1c7e91d41a89121a9162bfdaf7344ca13
Ameesha Jain <ameesha.jain@nutrabay.com>
2024-01-20 13:06:33 +0530

    added login api

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 import User from '../models/User.js'
 import bcrypt from 'bcryptjs'
+import { createError } from '../utils/error.js';
 
 export const register = async (req, res, next) => {
     try {
@@ -17,6 +18,24 @@ export const register = async (req, res, next) => {
 
         await newUser.save()
         res.status(200).json("New User Registered")
+
+    } catch (e) {
+        next(e)
+    }
+}
+
+export const login = async (req, res, next) => {
+    try {
+
+        const user = await User.findOne({
+            username: req.body.username,
+        })
+        if (!user) return next(createError(404, "User Not Found!"))
+
+        const is_password_correct = await bcrypt.compare(req.body.password, user.password)
+        if (!is_password_correct) return next(createError(404, "Wrong Password or Username!"))
+        const { password, is_admin, ...other_details } = user._doc
+        res.status(200).json(other_details)
 
     } catch (e) {
         next(e)
