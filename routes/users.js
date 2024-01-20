4f769bca119ce6c1a99cc1281d866e896fd48b7f
Ameesha Jain <ameesha.jain@nutrabay.com>
2024-01-20 22:43:58 +0530

    added verfify token middleware

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,11 @@
 import express from "express";
 import { deleteUser, getAllUser, getUser, updateUser } from "../controllers/user.js";
-
+import { verifyToken } from '../utils/verifyToken.js'
 const router = express.Router();
+
+router.get("/check_auth", verifyToken, (req, res, next) => {
+    res.send("Hi user, welcome back")
+})
 
 // update
 router.put("/:id", updateUser)
