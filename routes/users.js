8d6c558ea545347f2003ce80af2dd43f2efba4f8
Ameesha Jain <ameesha.jain@nutrabay.com>
2024-01-20 23:24:35 +0530

    added verifyUser and verifyAdmin middlewares

--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,31 @@
 import express from "express";
 import { deleteUser, getAllUser, getUser, updateUser } from "../controllers/user.js";
-import { verifyToken } from '../utils/verifyToken.js'
+import { verifyAdmin, verifyToken, verifyUser } from '../utils/verifyToken.js'
 const router = express.Router();
 
 router.get("/check_auth", verifyToken, (req, res, next) => {
-    res.send("Hi user, welcome back")
+    res.send("Hi user, you are authenticated!")
+})
+
+router.get("/check_user/:id", verifyUser, (req, res, next) => {
+    res.send("Hi user, you are authorized")
+})
+
+router.get("/check_admin/", verifyAdmin, (req, res, next) => {
+    res.send("Hi admin, you are authorized")
 })
 
 // update
-router.put("/:id", updateUser)
+router.put("/:id", verifyUser, updateUser)
 
 // delete
-router.delete("/:id", deleteUser)
+router.delete("/:id", verifyUser, deleteUser)
 
 // get
-router.get("/:id", getUser)
+router.get("/:id", verifyUser, getUser)
 
 // get all
-router.get("/", getAllUser)
+router.get("/", verifyAdmin, getAllUser)
 
 
 export default router
--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -1,5 +1,7 @@
 import jwt from "jsonwebtoken";
 import { createError } from "./error.js";
+import User from "../models/User.js";
+
 
 export const verifyToken = (req, res, next) => {
     const token = req.cookies.access_token
@@ -9,6 +11,30 @@ export const verifyToken = (req, res, next) => {
         if (err) return next(createError(403, "Invalid token"))
         req.user = user
         next()
+    })
+
+}
+
+export const verifyUser = (req, res, next) => {
+    verifyToken(req, res, () => {
+        if (req.user.id === req.params.id || req.user.is_admin) {
+            next()
+        }
+        else {
+            return next(createError(403, "You are not authorized"))
+        }
+    })
+
+}
+
+export const verifyAdmin = (req, res, next) => {
+    verifyToken(req, res, () => {
+        if (req.user.is_admin) {
+            next()
+        }
+        else {
+            return next(createError(403, "You are not authorized"))
+        }
     })
 
 }
